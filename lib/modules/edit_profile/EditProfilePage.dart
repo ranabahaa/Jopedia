bc23013e20ba35c13d8fe7d7565a3113a508b407
samaa <samaa.eiada@gmail.com>
2022-05-22 15:13:16 +0200

    job

--- lib/modules/edit_profile/EditProfilePage.dart
+++ lib/modules/edit_profile/EditProfilePage.dart
@@ -70,6 +70,8 @@ class _EditProfilePageState extends State<EditProfilePage> {
         TextEditingController(text: widget.user.email);
     final TextEditingController _controllerPhone =
         TextEditingController(text: widget.user.phone);
+    final TextEditingController _controllerNatonalId =
+        TextEditingController(text: widget.user.NatonalId);
 
 
     return Scaffold(
@@ -152,6 +154,26 @@ class _EditProfilePageState extends State<EditProfilePage> {
             ],
           ),
           const SizedBox(height: 24),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'NationalId',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: _controllerNatonalId,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                //maxLines: widget.maxLines,
+              ),
+            ],
+          ),
+          const SizedBox(height: 24),
           MaterialButton(
             color: Color(0xff50B3CF),
             minWidth: double.infinity,
@@ -174,6 +196,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
                 'name': _controllerName.text,
                 'email': _controllerEmail.text,
                 'phone': _controllerPhone.text,
+                'NatonalId': _controllerNatonalId.text,
               });
 
               ScaffoldMessenger.of(context).showSnackBar(
